fe7eb02f832d27985ba5bcb1ef44f11373eae7bf
KMulder-IGR <75676107+KMulder-IGR@users.noreply.github.com>
2020-12-20 17:06:05 +0100

    Update CerebroApp.R

--- Rscripts/Cerebro/CerebroApp.R
+++ Rscripts/Cerebro/CerebroApp.R
@@ -1,33 +1,27 @@
 
 
 library(Seurat)
-
-Covid = CreateSeuratObject(tissue_TPM,meta.data = selected_tpm,min.cells = 3)
-
-
 BiocManager::install('romanhaa/cerebroApp')
-
 if ( 'BiocManager' %in% installed.packages() == FALSE ) install.packages('BiocManager')
-
 library(BiocManager)
-
 if ( 'cerebroApp' %in% installed.packages() == FALSE ) install('cerebroApp')
 if ( 'SingleR' %in% installed.packages() == FALSE ) install('SingleR')
 if ( 'monocle' %in% installed.packages() == FALSE ) install('monocle')
-
 library(cerebroApp)
+
+integrated = readRDS("SeuratObject")
 
 exportFromSeurat(
   reference,
   assay = "integrated",
   slot = "data",
-  file = "Macroverse_Try.crb",
+  file = "Cerebro_Object.crb",
   experiment_name = "Macroverse",
   organism = 'hg',
   groups = c('Tissue','Phenograph','Global Healthy-1_Cancer-2_Other-3','Condition per tissue'),
   cell_cycle = NULL,
-  nUMI = "Patient No",
-  nGene = "Patient No",
+  nUMI = "nUMI",
+  nGene = "nGene",
   add_all_meta_data = TRUE,
   use_delayed_array = FALSE,
   verbose = FALSE
